1663f921c85a59005b75d93931338843c15f741c
Shubham <sk715152@gmail.com>
2024-08-17 11:14:30 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import { useState } from 'react';
-import './App.css';
-import Quiz from './utils/quizData';
-import Question from './components/Question';
+import { useState } from "react";
+import Quiz from "./utils/quizData";
+import Question from "./components/Question";
 
-function App() {
-const [currentQuestionIndex, setCurrentQuestionIndex]=useState(0);
-const cuurentQuestion = Quiz[currentQuestionIndex];
+const App = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
 
+  const currentQuestion = Quiz[currentQuestionIndex];
 
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setCorrectAnswersCount((prevCount) => prevCount + 1);
+    }
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='font-bold text-4xl fixed top-[20%]'>Quiz Game</h1>
-      <Question question={cuurentQuestion} setCurrentQuestionIndex={setCurrentQuestionIndex} currentQuestionIndex={currentQuestionIndex}/>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold text-4xl fixed top-[20%]">Quiz Game</h1>
+      <Question
+        question={currentQuestion}
+        setCurrentQuestionIndex={setCurrentQuestionIndex}
+        currentQuestionIndex={currentQuestionIndex}
+        onAnswer={handleAnswer}
+        correctAnswersCount={correctAnswersCount}
+        setCorrectAnswersCount={setCorrectAnswersCount}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Quiz from "../utils/quizData";
 
 const Question = ({
   question,
   currentQuestionIndex,
   setCurrentQuestionIndex,
+  onAnswer,
+  correctAnswersCount,
+  setCorrectAnswersCount,
 }) => {
   const [answer, setAnswer] = useState(null);
   const [quizComplete, setQuizComplete] = useState(false);
+  const [countdown, setCountdown] = useState(3);
+  const [isCountdownActive, setIsCountdownActive] = useState(false);
 
   const handleClick = (option) => {
+    const isCorrect = option === question.correctAnswer;
     setAnswer(option);
-    setTimeout(()=>{
-        setAnswer(null);
-    },2000);
+    onAnswer(isCorrect);
+    setIsCountdownActive(true);
   };
+
   const updateQuestion = () => {
     if (currentQuestionIndex < Quiz.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -22,63 +28,86 @@ const Question = ({
       setQuizComplete(true);
     }
     setAnswer(null);
+    setCountdown(3);
+    setIsCountdownActive(false);
   };
+
   const startAgain = () => {
     setAnswer(null);
     setCurrentQuestionIndex(0);
     setQuizComplete(false);
+    setCorrectAnswersCount(0);
+    setCountdown(5);
+    setIsCountdownActive(false);
   };
 
-  // Determine if the answer is correct
+  // Countdown effect
+  useEffect(() => {
+    let interval;
+    if (isCountdownActive && countdown > 0) {
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000);
+    } else if (countdown === 0) {
+      updateQuestion();
+    }
+    return () => clearInterval(interval);
+  }, [isCountdownActive, countdown]);
+
   const isCorrect = answer === question.correctAnswer;
 
   return (
     <div className="border rounded-lg p-4 text-center w-96 transition-all transform duration-500">
-        
-            {!quizComplete ? (
-                <div>
-                 <h1 className="font-semibold text-lg">{question.question}</h1>
-                 <div className="flex flex-col mt-4 items-center">
-                   {question.options.map((option) => (
-                     <button
-                       key={option}
-                       className={`text-black my-3 border w-full px-4 active:scale-x-[0.98] bg-slate-100 hover:bg-slate-300 transition-all transform duration-200`}
-                       onClick={() => handleClick(option)}
-                     >
-                       {option}
-                     </button>
-                   ))}
-                   {answer && (
-                     <h3
-                       className={`border px-4 py-[2px] font-semibold ${
-                         isCorrect ? "bg-green-300 text-black" : "bg-gray-400 text-black"
-                       } self-start`}
-                     >
-                       {isCorrect ? "Correct ✓" : "Incorrect X"}
-                     </h3>
-                   )}
-                 </div>
-                 {!quizComplete && (
-                   <button
-                     className="bg-blue-500 w-full mt-4 py-1 text-white font-semibold active:bg-blue-400 hover:scale-x-95 transition-all transform duration-300 active:scale-x-[0.97]"
-                     onClick={updateQuestion}
-                   >
-                     Next
-                   </button>
-                 )}
-                 <div/>
-                 </div>
-            ):(<div>
-                <h1 className="font-semibold text-lg">Quiz Complete</h1>
-                <button
-                     className="bg-blue-500 w-full mt-4 py-1 text-white font-semibold active:bg-blue-400 hover:scale-x-95 transition-all transform duration-300 active:scale-x-[0.97]"
-                     onClick={startAgain}
-                   >
-                     Start Again
-                   </button>
-            </div>)}
-        
-     
+      {!quizComplete ? (
+        <div>
+          <h1 className="font-semibold text-lg">{question.question}</h1>
+          <div className="flex flex-col mt-4 items-center">
+            {question.options.map((option) => (
+              <button
+                key={option}
+                className={`text-black my-3 border w-full px-4 active:scale-x-[0.98] bg-slate-100 hover:bg-slate-300 transition-all transform duration-200`}
+                onClick={() => handleClick(option)}
+              >
+                {option}
+              </button>
+            ))}
+            {answer && (
+              <h3
+                className={`border px-4 py-[2px] font-semibold ${
+                  isCorrect
+                    ? "bg-green-300 text-black"
+                    : "bg-gray-400 text-black"
+                } self-start`}
+              >
+                {isCorrect ? "Correct ✓" : "Incorrect X"}
+              </h3>
+            )}
+          </div>
+          {!quizComplete && (
+            <button
+              className="bg-blue-500 w-full mt-4 py-1 text-white font-semibold active:bg-blue-400 hover:scale-x-95 transition-all transform duration-300 active:scale-x-[0.97]"
+              onClick={updateQuestion}
+            >
+              {isCountdownActive
+                ? `Next Question In: ${countdown}s`
+                : "Skip Question"}
+            </button>
+          )}
+        </div>
+      ) : (
+        <div>
+          <h1 className="font-semibold text-lg">Quiz Complete</h1>
+          <h1 className="font-semibold text-base pt-4">
+            Total: {correctAnswersCount}
+          </h1>
+          <button
+            className="bg-blue-500 w-full mt-4 py-1 text-white font-semibold active:bg-blue-400 hover:scale-x-95 transition-all transform duration-300 active:scale-x-[0.97]"
+            onClick={startAgain}
+          >
+            Start Again
+          </button>
+        </div>
+      )}
     </div>
   );
 };
